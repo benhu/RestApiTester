25bfd0679e04ca4de6bb54532ed4824816e7f50d
Pakito <benjamin@pakitow.eu>
2016-03-10 22:18:11 +0000

    Test de Travis CI

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ rm -rf out || exit 0;
 mkdir out;
 
 # go to the out directory and create a *new* Git repo
-cd out
+(cd out
 git init
 
 # inside this git repo we'll pretend to be a new user
@@ -19,7 +19,7 @@ mv example.data.json data.json
 
 # The first and only commit to this new Git repo contains all the
 # files present with the commit message "Deploy to GitHub Pages".
-git add -A
+git add .
 git commit -m "Deploy to GitHub Pages"
 
 # Force push from the current repo's master branch to the remote
@@ -27,3 +27,4 @@ git commit -m "Deploy to GitHub Pages"
 # will be lost, since we are overwriting it.) We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+)
