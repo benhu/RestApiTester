d37f810e8f017b07699592e60723a21c1778da9c
Pakito <benjamin@pakitow.eu>
2016-04-04 22:02:35 +0000

    Separation des libs vendors et app

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,20 @@
 // Gulp Dependencies
 var gulp = require('gulp'),
-  rename = require('gulp-rename'),
 
-  // Build Dependencies
-  browserify = require('gulp-browserify'),
-  uglify = require('gulp-uglify'),
-  jshint = require('gulp-jshint'),
+    // Build Dependencies
+    browserify = require('browserify'),
+    uglify = require('gulp-uglify'),
+    jshint = require('gulp-jshint'),
+    source = require('vinyl-source-stream'),
 
-  // Style Dependencies
-  minifyCSS = require('gulp-minify-css'),
+    // Style Dependencies
+    minifyCSS = require('gulp-minify-css'),
 
-  // React Dependencies
-  babelify = require("babelify");
+    // React Dependencies
+    babelify = require("babelify");
+
+var packageJson = require('./package.json');
+var dependencies = Object.keys(packageJson && packageJson.dependencies || {});
 
 gulp.task('default', ['watch']);
 
@@ -31,27 +34,40 @@ gulp.task('copy-image', function() {
   return gulp.src('src/image/**/*').pipe(gulp.dest('build/assets/image'));
 });
 
-gulp.task('browserify-client', function() {
-  return gulp.src('src/javascript/main.js')
-    .pipe(browserify({
-      debug: true,
-      transform: babelify.configure({
-        presets: ['es2015', 'react']
-      })
-    }))
-    .pipe(rename('bundle.js'))
+gulp.task('vendors', function () {
+    return browserify({
+            debug: false,
+            require: dependencies
+        }).bundle()
+          .pipe(source('vendors.js'))
+          .pipe(gulp.dest('build/assets/javascript'));
+});
+
+gulp.task('browserify', function() {
+  var stream = browserify({
+    debug: true,
+    entries: ['src/javascript/main.js'],
+    transform: babelify.configure({
+      presets: ['es2015', 'react']
+    })
+  });
+
+  stream.external(dependencies);
+
+  return stream.bundle()
+    .pipe(source('app.js'))
     .pipe(gulp.dest('build/assets/javascript'));
 });
 
-//gulp.task('jshint', function() {
-//  return gulp.src('src/javascript/**/*.js')
-//    .pipe(babelify.configure({presets: ['es2015', 'react']}))
-//    .pipe(jshint())
-//    .pipe(jshint.reporter('jshint-stylish'));
-//});
+gulp.task('jshint', function() {
+  return gulp.src('src/javascript/**/*.js')
+    .pipe(babelify.configure({presets: ['es2015', 'react']}))
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
 
-gulp.task('uglify', ['browserify-client'], function() {
-  return gulp.src('build/assets/javascript/bundle.js')
+gulp.task('uglify', ['browserify', 'vendors'], function() {
+  return gulp.src('build/assets/javascript/*.js')
     .pipe(uglify())
     .pipe(gulp.dest('build/assets/javascript'));
 });
@@ -63,7 +79,8 @@ gulp.task('minify', ['copy-css'], function() {
 });
 
 gulp.task('watch', function() {
-  gulp.watch('src/javascript/**/*.js', [ /*'jshint',*/ 'browserify-client']);
+  gulp.watch('package.json', ['vendors']);
+  gulp.watch('src/javascript/**/*.js', [ 'jshint', 'browserify']);
   gulp.watch('src/css/**/*.css', ['copy-css']);
   gulp.watch('src/**/*.html', ['copy-html']);
   gulp.watch('src/image/**/*', ['copy-image']);
